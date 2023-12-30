7162eea956dfeeaa49d94b7eae8e0c8f5e348f9a
Jose T <josetorresgarrido@gmail.com>
2023-12-30 17:23:45 -0500

    reactor

--- src/main/java/com/reactorjava/app/App.java
+++ src/main/java/com/reactorjava/app/App.java
@@ -1,11 +1,7 @@
 package com.reactorjava.app;
 
-/**
- * Hello world!
- *
- */
 public class App {
     public static void main( String[] args ) {
-        System.out.println( "Hello World!" );
+
     }
 }
